f515faf38da1f4596f51b806432775249ffd5255
alnicolaou96 <alnicolaou96@gmail.com>
2018-05-30 16:01:37 -0400

    first commit

---
+++ SuperHeroOOP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name,superpower;
+            int strength, speed;
+
+            Console.WriteLine("whats the name of your super hero");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your super hero's strength level?");
+            strength = int parse(Console.ReadLine());
+
+
+            SuperHero batman = new SuperHero();
+            batman.SetName(name);
+
+        }
+    }
+}
---
+++ SuperHeroOOP/SuperHero.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroOOP
+{
+    class SuperHero
+    {
+        //declare fields
+       private string name, superPower;
+       private int strength, speed;
+
+
+        //declare properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public string SuperPower
+        {
+            get { return this.superPower; }
+            set { this.superPower = value; }
+        }
+        public int Strength
+        {
+            get { return this.strength; }
+            set { this.strength = value; }
+        }
+        public int Speed
+        {
+            get { return this.speed; }
+            set { this.speed = value; }
+        }
+
+        //constructors
+        public SuperHero()
+        {
+            //default constructor
+        }
+
+        public SuperHero(int strength, int speed)
+        {
+            //loaded constructor
+            this.strength = strength;
+            this.speed = speed;
+        }
+
+        public SuperHero(string name, string superPower)
+        {
+            //loaded constructor
+            this.name = name;
+            this.superPower=superPower;
+        }
+
+        public void HowFast()
+        {
+        }
+        public void SetName(string name)
+        {
+            //this.name = name;
+            Console.WriteLine("The Name of the super hero is " + name);
+        }
+
+
+    }
+}
